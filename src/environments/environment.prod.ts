4e90402260700cc11f98e94e8827bb15b7156f91
HimaniGaur <himanigaur1998@gmail.com>
2020-06-08 07:19:44 +0530

    env for firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyBXs3a_zAcvBK6bG47L2IMEbgudZPxaBgE",
+  authDomain: "planetapp-8f602.firebaseapp.com",
+  databaseURL: "https://planetapp-8f602.firebaseio.com",
+  projectId: "planetapp-8f602",
+  storageBucket: "planetapp-8f602.appspot.com",
+  messagingSenderId: "822692617139",
+  appId: "1:822692617139:web:9d0a95ea598264042b09c6",
+  measurementId: "G-Z02JKJ3XER"
+  }
 };
