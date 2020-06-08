b0864b2a39c74f637aa3aa23dff6b1d7d0e2681e
HimaniGaur <himanigaur1998@gmail.com>
2020-06-08 06:05:56 +0530

    list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatTabsModule} from '@angular/material/tabs';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PlanetsComponent } from './components/planets/planets.component';
+import { FavouritesComponent } from './components/favourites/favourites.component';
+
+const appRoutes: Routes = [
+  { path: 'planets', component: PlanetsComponent },
+  { path: 'favourites', component: FavouritesComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlanetsComponent,
+    FavouritesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
+    MatTabsModule,
+    HttpClientModule,
+    RouterModule
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
